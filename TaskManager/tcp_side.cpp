abd795aa6a489b9c0ad4024d24526e9be00831d0
TrueCraftWorld <truecraftworld@gmail.com>
2024-05-08 23:27:06 +0500

    file clean-up

--- TaskManager/tcp_side.cpp
+++
@@ -1,7 +0,0 @@
-#include "tcp_side.h"
-
-TCP_Side::TCP_Side(QObject *parent)
-    : QObject{parent}
-{
-
-}
--- TaskManager/udp_side.cpp
+++
@@ -1,7 +0,0 @@
-#include "udp_side.h"
-
-UDP_Side::UDP_Side(QObject *parent)
-    : QObject{parent}
-{
-
-}
